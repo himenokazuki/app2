3893dba823cf756af370cbd1a3ad93648a8425cf
himenokazuki <129943270+himenokazuki@users.noreply.github.com>
2023-05-01 15:56:06 +0000

    課題２途中

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,43 @@
+class BooksController < ApplicationController
+  def new
+     # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
+    @book = Book.new  
+  end
+
+# 以下を追加
+  def create
+     @book = Book.new(book_params)
+    if @book.save
+       flash[:notice] = "successfully "
+      redirect_to book_path(@book.id)
+    else
+      @books=Book.all
+      render  :index
+    end
+  end
+  def index
+   @book = Book.new 
+   @books =Book.all
+   
+  end
+  
+  def show
+  @book=Book.find(params[:id]) 
+  end 
+  
+  def edit
+  @book=Book.find(params[:id])
+  end  
+  
+  def destroy
+    book=Book.find(params[:id])
+    book.destroy
+    redirect_to'/books'
+  end
+  
+  private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
